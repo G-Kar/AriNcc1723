8cf717a30ccc289434ddb65311ade9b4d4fad421
Ender <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2018-11-07 16:52:05 +1300

    Create LoadingModule0.py

---
+++ ENDER-PROGS/LoadingModule0.py
@@ -0,0 +1,16 @@
+# Loading Module | EnderNightLord |
+import time
+
+time.sleep(0.1)
+print('1% ->')
+time.sleep(0.1)
+print('2% -->')
+time.sleep(0.1)
+print('3% --->')
+time.sleep(5)
+print('4% ---->')
+time.sleep
+print('100% ---------------------------------------------------------------------------------------------------->')
+
+
+
