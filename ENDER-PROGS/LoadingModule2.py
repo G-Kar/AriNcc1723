7be65bb1bc10f59ce10dcb741bd14c2666ac5da5
Ender <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2018-11-08 16:51:40 +1300

    Update LoadingModule2.py

--- ENDER-PROGS/LoadingModule2.py
+++ ENDER-PROGS/LoadingModule2.py
@@ -1,12 +1,12 @@
 import time
 
-while True
-print('/')
-time.sleep
-print('-')
-time.sleep
-print('\')
-time.sleep
-primt('|')
-time.sleep
-print('/')
+while True:
+  print('/')
+  time.sleep
+  print('-')
+  time.sleep
+  print('\')
+  time.sleep
+  primt('|')
+  time.sleep
+  print('/')
